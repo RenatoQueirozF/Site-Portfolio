0242c8124f7f118f5e20c95fa414380ec747eb5f
Renato Queiroz <renatoqf.13@gmail.com>
2022-02-10 15:11:09 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,55 @@
+//Scroll Suave
+
+
+//Definir todos os itens da navbar como variavel
+const navMenu = document.querySelectorAll ('.nav-item[href^="#"]');
+
+//Adicionar evento para todos os elementos da Navbar ao clicar
+navMenu.forEach(item => {
+  item.addEventListener('click', ScrollToIdOnClick);
+})
+//Evento ao clicar
+function ScrollToIdOnClick (event) {
+  event.preventDefault();
+  const element = event.target;
+  const id = element.getAttribute('href');
+  const to = document.querySelector (id).offsetTop;
+  
+//Funcionamento do Scroll Suave
+  window.scroll ({
+
+    top: to,
+    behavior: "smooth",
+  });
+}
+
+//Carrossel
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Controle Next / Prev
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Controle das imagens
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("carrossel-imagens");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
